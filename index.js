14e4125f00fc97a3302022a12f25819d36f0e55f
Takkino31 <takkinoyaya@gmail.com>
2022-07-28 12:10:25 +0000

    test done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const packageName = require('express');
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const {DB_USERNAME,DB_PASS,MODE_ENV,PORT} = process.env
+
+
+app.get('/test' , (req , res)=>{
+
+   res.send('hello from simple server :)')
+
+})
+
+console.log(PORT);
+
+app.listen(PORT , ()=> {
+    console.log('> Server is up and running on port : ' + PORT)
+})
+
+console.log(DB_USERNAME);
