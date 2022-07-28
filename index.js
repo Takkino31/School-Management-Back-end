e71a98f98ba2d842f95ae47a5b85610ae29d7a41
Takkino31 <takkinoyaya@gmail.com>
2022-07-28 13:41:08 +0000

    training with routes on index.js before structuring the project

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
-const app = express()
 require('dotenv').config()
+const Student = require('./student/student')
+
 const {DB_USERNAME,DB_PASS,MODE_ENV,PORT} = process.env
 
+const app = express()
+app.use(express.json())
 
-app.get('/test' , (req , res)=>{
+app.get('/students' , (req , res)=>{
+    const students = Student.findAllStudents()
+    res.send(students)
+})
 
-   res.send('hello from simple server :)')
+app.get('/student/:id' , (req , res)=>{
+    const student = Student.findOneStudent(req.params.id)
+    res.send(req.body)
+})
 
+app.post('/student',(req,res) =>{
+    console.log(req.body)
+    Student.addStudent(req.body)
+    res.send(req.body)
+})
+
+app.put('/student/:id',(req,res) =>{
+    console.log(req.body)
+    Student.updateStudent(req.params.id,req.body)
+    res.send(req.body)
 })
 
 console.log(PORT);
@@ -15,5 +34,6 @@ console.log(PORT);
 app.listen(PORT , ()=> {
     console.log('> Server is up and running on port : ' + PORT)
 })
+
 
 console.log(DB_USERNAME);
