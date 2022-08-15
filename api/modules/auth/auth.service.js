ee939ef2aa0d74b89d7e9448fd4ada98c47ad8e6
Takkino31 <takkinoyaya@gmail.com>
2022-08-15 22:50:30 +0000

    token done

--- api/modules/auth/auth.service.js
+++ api/modules/auth/auth.service.js
@@ -1,13 +1,21 @@
 const UserService = require('../users/users.service')
-
+const jwt = require('jsonwebtoken')
 
 module.exports.register = ({username,password}) =>{
     UserService.insertOne({username,password})
 }
 
 module.exports.login = ({username,password}) =>{
-    const user = UserService.findOneBysername(username)
-    return (!user || user.password !== password) ? null : user
+    let user = UserService.findOneBysername(username)
+    if (!user || user.password !== password) {
+        return null
+    } else {
+        user = {...user}
+        console.log(user);
+        delete user.password
+        const token = jwt.sign(user,'myAppKey',{expiresIn: 60*60})
+        return {user, token}
+    }
 }
 
 module.exports.listUsers = ()=>{
