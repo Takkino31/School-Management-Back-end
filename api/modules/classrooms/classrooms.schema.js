f52008187b020775e1f59dde8ad4d997259af0fe
Takkino31 <takkinoyaya@gmail.com>
2022-09-21 12:37:59 +0000

    Routes Classrooms (ADD EDIT DELETE LISTING) Done

---
+++ api/modules/classrooms/classrooms.schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const classroomSchema = new mongoose.Schema({
+    className: {type: String},
+    desc: {type: String}
+})
+
+const model = mongoose.model('classroom',classroomSchema)
+module.exports = model
